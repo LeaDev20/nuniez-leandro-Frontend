de4be861fbe8eac9f2623cced9863ac4feccad56
Alan 21 Code <alan21naci@gmail.com>
2020-10-03 18:01:01 -0300

    Updates

---
+++ precache-manifest.d92850b9f8525108e4151455a7e7942b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f02b8a3023193406a09cb2b0ee29591",
+    "url": "/nuniez-leandro-Frontend/index.html"
+  },
+  {
+    "revision": "b5a1affeb77563d9c15d",
+    "url": "/nuniez-leandro-Frontend/static/css/main.21f09730.chunk.css"
+  },
+  {
+    "revision": "6841d318d5b1e02d3123",
+    "url": "/nuniez-leandro-Frontend/static/js/2.6604af16.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nuniez-leandro-Frontend/static/js/2.6604af16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a1affeb77563d9c15d",
+    "url": "/nuniez-leandro-Frontend/static/js/main.a9f2910a.chunk.js"
+  },
+  {
+    "revision": "16ce199185dbfa5e52cd",
+    "url": "/nuniez-leandro-Frontend/static/js/runtime-main.03dc755c.js"
+  }
+]);
